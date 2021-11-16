330b0663376187c0fd2f453d9cd681cd5cb352a4
Neil Smith <s3nt1n3lz18@gmail.com>
2021-11-16 17:04:44 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export type cacheLastTimeout = Map<Function, 
+  {
+
+    timeout: 
+  }
+>;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cache-last-timeout';
+
+  // Cache the last value, get a new value every timeout, or retrieve early if retrieveNow = true
+  // Used for caching api responses or 
+  cacheLastTimeout = (func, timeout, retrieveNow) => {
+    const results = {};
+    const last = null;
+    return (...args) => {
+      const argsKey = JSON.stringify(args);
+      if (!results[argsKey] || Date.now() - last > timeout || retrieveNow) {
+        results[argsKey] = func(...args);
+      }
+      return results[argsKey];
+    };
+  };
+
+  valueChanges() {
+
+  }
+
+  ngOnInit() {
+    console.time();
+
+    console.timeEnd();
+  }
 }
